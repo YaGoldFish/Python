f9b6b1c15df92a638bc6f06843d6ea0007d7dc73
Christian Quinones <12quinonesc@gmail.com>
2020-10-16 13:22:42 -0400

    Initial Commit

---
+++ Hello.py
@@ -0,0 +1,25 @@
+import os
+os.system("clear")
+
+
+
+full_name = "christian quinones"
+
+print (full_name)
+age = 21
+names = ["christian", "nikki"]
+
+print (names)
+
+
+fav_food = {
+	"christian": "soup dumplings",
+	"nikki": "corn dogs", 
+}
+print (fav_food["christian"])
+
+
+
+# print hello world to the scree 
+print ("Hello World")
+
